f3aac13748d878d67482555d3e0aed7e6ea0b299
gasser-ma <57869461+gasser-ma@users.noreply.github.com>
2023-07-31 19:54:44 +0200

    Update cutencoding.py

--- cutencoding.py
+++ cutencoding.py
@@ -53,14 +53,6 @@ class set_cut_encodings(inkex.Effect):
         cut_types_stroke_colors = {"int":"#000000", "ext":"#000000", "online":"#969696","poc":"#969696","guide_f":"#0000ff","guide_s":"#0000ff"}
         cut_types_fill_colors = {"int":"none", "ext":"#000000", "online":"none","poc":"#969696","guide_f":"#0000ff","guide_s":"none"}
         units = {"1":"mm","2":"in"}
-        if self.options.tab == "check_path":
-            for elem in self.svg.selection.filter(inkex.PathElement):
-                tagD = elem.get("d")
-                #messagebox.showwarning("Warning", tagD)
-                checked = self.check_path_closed(tagD,True)
-                elem.set("d",checked["tag_d"])
-                pass
-            pass
 
         if self.options.tab == "page_cut_encoding":
             #
@@ -100,67 +92,6 @@ class set_cut_encodings(inkex.Effect):
         #
         pass
 
-    def check_path_closed(self,svg_tag_d, fix_it):
-        #
-        ret = {"tag_d":svg_tag_d,"end_Z": True}
-        tag_d_fixed = svg_tag_d
-        #
-        startM = False
-        tag_d_counter = 0
-        #endeZ = False
-        m2z_error = []
-        for c in svg_tag_d:
-            if c=="m" or c=="M":
-                if startM == True:
-                    # darf nicht true sein denn da fehlt ein ende Z
-                    # hier könnte ein fix eingebaut werden
-                    m2z_error.append(tag_d_counter)
-                    pass
-                else:
-                    startM = True
-                    pass
-                pass
-            else:
-                pass
-            if c=="z" or c=="Z":
-                if startM == True:
-                    #endeZ = True
-                    startM = False
-                    pass
-                else:
-                    #
-                    pass
-                pass
-            else:
-                pass
-            tag_d_counter += 1
-            # next c
-            pass
-        #kontrolle auf ende Z
-        if startM == True:
-            # start ist immer noch true-- somit kein Ende Z
-            m2z_error.append(tag_d_counter)
-            pass
-        else:
-            pass
-        messagebox.showinfo("inof","erros on " + str(m2z_error))
-        #
-        if fix_it == True:
-            if len(m2z_error) > 0:
-                m2z_error.sort(reverse=True) # umkehren von hinten beginnen
-                for i in m2z_error:
-                    tag_d_fixed = tag_d_fixed[:i] + " z " + tag_d_fixed[i:]
-                    pass
-                ret["tag_d"] = tag_d_fixed
-                pass
-            else:
-                pass
-            pass
-        else:
-            pass
-        messagebox.showinfo("inof","fixed " + str(tag_d_fixed))
-        return ret
-        pass
 
 if __name__ == '__main__':
     set_cut_encodings().run()
